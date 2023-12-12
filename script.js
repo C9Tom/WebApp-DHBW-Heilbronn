2586c77eec0f76fe860d1926ac2bbe518a0031d6
C9Tom <tomplieninger1@web.de>
2023-12-12 11:20:24 +0100

    changed script and styles file. Tried to implement the emailvalidation api. Created all Sites used in the the website

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// script.js
 
+$(document).ready(function () {
+    $('#emailForm').submit(function (e) {
+      e.preventDefault();
+  
+      var apiKey = 'ema_live_ofh534E68GDclPMzdp70Gltfwr45BhrMWfxe8Xaf';
+      var email = $('#emailInput').val();
+  
+      $.ajax({
+        type: 'GET',
+        url: 'https://api.emailvalidation.io/verify',
+        data: { apiKey: apiKey, email: email },
+        success: function (response) {
+          // Verarbeite die API-Antwort hier
+          console.log(response);
+  
+          // Beispiel: Zeige das Ergebnis auf der Website an
+          $('#result').text('Ergebnis: ' + response.result);
+        },
+        error: function (error) {
+          console.log('Fehler bei der API-Anfrage: ' + JSON.stringify(error));
+        }
+      });
+    });
+  });
+  

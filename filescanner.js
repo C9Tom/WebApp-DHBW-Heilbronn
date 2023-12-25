e8a85785e616fcbfcdfb7a5f508e56de615c1edc
C9Tom <tomplieninger1@web.de>
2023-12-25 17:41:46 +0100

    small changes

--- filescanner.js
+++ filescanner.js
@@ -3,7 +3,7 @@ const options = {
     headers: {accept: 'text/plain', 'content-type': 'application/json'},
     body: JSON.stringify({
       apikey: '85f5e2f04ae45a7bed752aa3727378bc222a3fc63189b56497abdb8939fb0f82',
-      file: 'string'
+      file: ''
     })
   };
   

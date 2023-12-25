04c6f9247f0e9a8c63c6e2aa2707777c154ca81e
C9Tom <tomplieninger1@web.de>
2023-12-25 17:24:54 +0100

    removed axios because of switch to fetch

--- filescanner.js
+++ filescanner.js
@@ -1,17 +1,13 @@
-import axios from 'axios';
-
 const options = {
-  method: 'POST',
-  url: 'https://www.virustotal.com/vtapi/v2/file/scan',
-  headers: {accept: 'text/plain', 'content-type': 'application/json'},
-  data: {apikey: '85f5e2f04ae45a7bed752aa3727378bc222a3fc63189b56497abdb8939fb0f82'}
-};
-
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+    method: 'POST',
+    headers: {accept: 'text/plain', 'content-type': 'application/json'},
+    body: JSON.stringify({
+      apikey: '85f5e2f04ae45a7bed752aa3727378bc222a3fc63189b56497abdb8939fb0f82',
+      file: 'string'
+    })
+  };
+  
+  fetch('https://www.virustotal.com/vtapi/v2/file/scan', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
